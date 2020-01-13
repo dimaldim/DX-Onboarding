870e17a5ce501cab0dd3120f571055e4c57f2caf
dimaldim <ddimitrov@devrix.com>
2020-01-13 16:08:07 +0200

    Change: Layout Changed layout, moved information about post below the title.

--- themes/twentysixteen/template-parts/content-single.php
+++ themes/twentysixteen/template-parts/content-single.php
@@ -11,6 +11,17 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<p>
+			<?php
+			printf(
+				'%1$s <a href="%2$s">%3$s</a> on %4$s',
+				_x( 'Posted by', '', 'twentisixteen' ),
+				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+				get_the_author(),
+				get_the_date()
+			);
+			?>
+		</p>
 	</header><!-- .entry-header -->
 
 	<?php
@@ -47,17 +58,17 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php twentysixteen_entry_meta(); ?>
+		<?php //twentysixteen_entry_meta(); ?>
 		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Post title. */
-					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
+//			edit_post_link(
+//				sprintf(
+//					/* translators: %s: Post title. */
+//					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+//					get_the_title()
+//				),
+//				'<span class="edit-link">',
+//				'</span>'
+//			);
 			?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
