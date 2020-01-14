818b6379c9264f72be751d86ff92499d2eec3b77
dimaldim <ddimitrov@devrix.com>
2020-01-14 12:25:04 +0200

    Init: Amazon plugin Beginning creation of amazon plugin.

---
+++ plugins/amazon-plugin/amazon-plugin.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: Amazon plugin
+ * Description: Display results from provided Amazon link.
+ * Version: 1.0.0
+ * Author: Dimitar Dimitrov
+ * Author URI: http://devrix.com/
+ * Text Domain: dx-amazon
+ * Domain Path: /languages
+ * License: GPL2
+ *
+ * @package Amazon_Plugin
+ */
+
+if ( ! class_exists( 'DX_Amazon' ) ) {
+	/**
+	 * Class DX_Amazon
+	 */
+	class DX_Amazon {
+		/**
+		 * DX_Amazon constructor.
+		 */
+		public function __construct() {
+		}
+	}
+}
+
+new DX_Amazon();
