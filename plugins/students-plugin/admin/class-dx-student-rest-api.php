ba8bd6cc3f7293401227e60ce3d84435ba19ce71
dimaldim <ddimitrov@devrix.com>
2020-01-15 16:38:41 +0200

    Feature: REST API get student by ID Added ability to get student by provided ID from the REST API.

--- plugins/students-plugin/admin/class-dx-student-rest-api.php
+++ plugins/students-plugin/admin/class-dx-student-rest-api.php
@@ -10,6 +10,42 @@ if ( ! class_exists( 'DX_Student_Rest' ) ) {
 		 */
 		public function __construct() {
 			add_action( 'rest_api_init', array( $this, 'dx_rest_student_get_all' ) );
+			add_action( 'rest_api_init', array( $this, 'dx_rest_student_get_by_id' ) );
+		}
+
+		/**
+		 * REST API register custom route to get student by id.
+		 */
+		public function dx_rest_student_get_by_id() {
+			register_rest_route(
+				'student/v1',
+				'/student/(?P<id>\d+)',
+				array(
+					'method'   => 'GET',
+					'callback' => array( $this, 'handle_get_student_by_id' ),
+				)
+			);
+		}
+
+		/**
+		 * Handle get student by ID.
+		 *
+		 * @param WP_REST_Request $data data.
+		 *
+		 * @return array|string|void|WP_Post
+		 */
+		public function handle_get_student_by_id( $data ) {
+			if ( ! $data->has_valid_params() ) {
+				return $data->has_valid_params();
+			}
+
+			$student = get_post( (int) $data['id'] );
+
+			if ( empty( $student ) || 'student' !== $student->post_type ) {
+				return __( 'No student found with the provided ID.', 'dx-students' );
+			}
+
+			return $student;
 		}
 
 		/**
