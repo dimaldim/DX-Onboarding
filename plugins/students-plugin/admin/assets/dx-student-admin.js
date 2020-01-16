a9d3fc5bcb2f5a4ee71ff9be5db2d7b798ad084c
dimaldim <ddimitrov@devrix.com>
2020-01-16 17:26:28 +0200

    Refactor: Student plugin Refactored student plugin admin js file.

--- plugins/students-plugin/admin/assets/dx-student-admin.js
+++ plugins/students-plugin/admin/assets/dx-student-admin.js
@@ -1,36 +1,28 @@
-jQuery(document).ready(function($) {
-	$('.dx-student-datepicker').datepicker();
+jQuery( document ).ready( ( $ ) => {
+	// Date picker for Birth day custom meta.
+	$( '.dx-student-datepicker' ).datepicker();
 
-	let student_checkboxes = $('.column-student_active input[type="checkbox"]');
-	student_checkboxes.each(function() {
-		let current_checkbox = $(this);
-		current_checkbox.change(function() {
-			let student_id = $(this).data('studentId');
+	// Listen for a click event on the checkboxes in column 'Enabled'
+	$( '.dx-student-status' ).on( 'click', function () {
+		const student_id = $( this ).data( 'studentId' );
+		change_student_status( student_id );
+	} );
 
-			change_student_status(student_id);
-		});
-	});
-
-	function clear_old_notices() {
-		let notices = $('#dx-student-notice');
-		if(notices.length > 0) {
-			notices.remove();
-		}
-	}
-
-	function change_student_status(student_id) {
-		var data = {
-			'action': 'dx_student_change_status',
-			'student_id': student_id,
-			'_nonce': dx_student_ajax_object._ajax_nonce
+	// Change student status
+	function change_student_status( student_id ) {
+		const data = {
+			action: 'dx_student_change_status',
+			student_id,
+			_nonce: dx_student_ajax_object._ajax_nonce
 		};
 		$.post(
 			dx_student_ajax_object.ajax_url,
 			data,
-			function (response) {
-				clear_old_notices();
-				$('<div id="dx-student-notice" class="updated notice">Student updated!</div>').insertAfter('.wp-heading-inline').fadeOut(3000);
+			( response ) => {
+				$( '<div id="dx-student-notice" class="updated notice">Student updated!</div>' )
+					.insertAfter( '.wp-heading-inline' )
+					.fadeOut( 3000 );
 			}
 		);
 	}
-});
+} );
